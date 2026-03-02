6b8c11d0229b5ab6ba3e11d006e6c38996d0d829
Bluecode77732 <doookuuu@gmail.com>
2026-03-02 20:59:39 +0900

    Debug - Solving on 'Cannot Find Sender ID': Seems jwt strategy passport cannot populates eq.user, so GraphQL context cannot find sender id.

--- src/auth/guard/graphql.auth.guard.ts
+++ src/auth/guard/graphql.auth.guard.ts
@@ -7,14 +7,26 @@ export class GraphQLAuthGuard extends AuthGuard('jwt-auth-guard') {
     getRequest(context: ExecutionContext) {
         const GqlCtx = GqlExecutionContext.create(context);
         const ctx = GqlCtx.getContext()
-        //! Debug - Solving on 'Cannot Find Sender ID': `|| 1`
-        const req = ctx.req || { headers: { authorization: ctx.authorization } } || 1;
+        //! Debug - Solving on 'Cannot Find Sender ID': Seems jwt strategy passport cannot populates `req.user`, so GraphQL context cannot find sender id.
+        // const req = ctx.req || { headers: { authorization: ctx.authorization } } || 1;
+        const token = ctx.req?.headers?.authorization || 1;
+        console.log(token);
+
+        const tokenSplit = token.split(' ')[1].split('.')[1];
+        console.log(tokenSplit);
+        
+        const payload = JSON.parse(Buffer.from(tokenSplit, 'base64').toString('utf-8'));
+        console.log(payload);
+        
+        const userId = payload.sub;
+        console.log(userId);
 
         // Flow logging
         // console.log('GraphQL Context:', ctx);
         // console.log('Request object:', req);
         // console.log('Authorization header:', req?.headers?.authorization);
 
-        return req;
+        // return req;
+        return userId;
     };
 };
