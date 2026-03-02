c00f2960a04d8e6e20e27286dfd41ab38fee2e3a
Bluecode77732 <doookuuu@gmail.com>
2026-03-02 17:47:20 +0900

    Debug: Solving on 'Cannot Find Sender ID'

--- src/auth/guard/graphql.auth.guard.ts
+++ src/auth/guard/graphql.auth.guard.ts
@@ -7,6 +7,7 @@ export class GraphQLAuthGuard extends AuthGuard('jwt-auth-guard') {
     getRequest(context: ExecutionContext) {
         const GqlCtx = GqlExecutionContext.create(context);
         const ctx = GqlCtx.getContext()
+        //! Debug: Solving on 'Cannot Find Sender ID'
         const req = ctx.req || { headers: { authorization: ctx.authorization } } || 1;
 
         // Flow logging
